4d2d16a60ae47193d93a6b7cdfb92b96c88f014c
EchchaaraouiAkilek <ech.chaaraouiamine@gmail.com>
2024-08-20 11:10:50 +0100

    hello

--- client/src/components/GetAllClients.tsx
+++ client/src/components/GetAllClients.tsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 import Loader from '../common/Loader';
-import { toast, ToastContainer } from 'react-toastify';
+import { toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 
