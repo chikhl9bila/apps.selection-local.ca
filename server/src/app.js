ad3033b333bdece67cf685707f55fb91ca1eda18
EchchaaraouiAkilek <ech.chaaraouiamine@gmail.com>
2024-08-25 13:28:55 +0100

    try

--- server/src/app.js
+++ server/src/app.js
@@ -1,20 +1,22 @@
-const express = require("express")
-const bodyParser = require("body-parser")
-const clientRoute = require("./routes/consultantRoute")
+const express = require("express");
+const bodyParser = require("body-parser");
+const clientRoute = require("./routes/consultantRoute");
 const cors = require('cors');
+const path = require('path'); // Import path module
 
-
-
-const app = express()
+const app = express();
 app.use(cors());
-
-
-app.use(express.static("public"))
-
 app.use(bodyParser.json({ limit: '10mb' }));
 
+// Serve API routes
+app.use("/api/consultant", clientRoute);
 
-app.use("/api/consultant" , clientRoute) ; 
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, 'public')));
 
+// Catch-all route to serve the React app
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 
-module.exports = app ;  
+module.exports = app;
